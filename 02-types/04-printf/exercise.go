5f3afb7f95a566bbcbf166a439c45ae644c23580
Livi Onozo <livia.onozo@barcelonagse.eu>
2024-09-14 18:15:51 +0200

    second exercise printf solved

--- 02-types/04-printf/exercise.go
+++ 02-types/04-printf/exercise.go
@@ -7,4 +7,30 @@ import (
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func printBool(b bool) string {
+	return fmt.Sprintf("variable of type boolean and value %v", b)
+}
+
+func printInt(i int) string {
+	return fmt.Sprintf("variable of type integer and value %v", i)
+}
+
+func printHex(i int) string {
+	return fmt.Sprintf("variable of type integer in hexadecimal form and value %x", i)
+}
+
+func printFloat(f float64) string {
+	return fmt.Sprintf("variable of type float and value %.2f", f)
+}
+
+func printString(s string) string {
+	return fmt.Sprintf("variable of type string and value %q", s)
+}
+
+func concatStrings(a, b string) string {
+	return a + b
+}
+
+func printConcatStrings(a, b string) string {
+	return printString(concatStrings(a, b))
+}
