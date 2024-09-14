7f3a394d9af143b7a8ce646f9100e71e15a8b0a1
Livi Onozo <livia.onozo@barcelonagse.eu>
2024-09-14 18:24:58 +0200

    3rd exercise repaint solved

--- 03-variables/01-repaint/exercise.go
+++ 03-variables/01-repaint/exercise.go
@@ -7,5 +7,11 @@ import "fmt"
 
 // repaintColor returns the complementary of the color received as argument or an error for an unknown error.
 func repaintColor(color string) (string, error) {
-	// INSERT YOUR CODE HERE
+	if color == "teal" {
+		return "vermilion", nil
+	}
+	if color == "vermilion" {
+		return "teal", nil
+	}
+	return "", fmt.Errorf("unknown color: %s", color)
 }
